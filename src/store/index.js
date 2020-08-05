760dc544b2f7002b8a66284be12e3b6b49fb3f4e
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2020-08-05 15:23:17 +0530

    Adds redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
-import {createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
+
+const logger = createLogger();
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  undefined,
+  applyMiddleware(logger),
 );
 
 export default store;
